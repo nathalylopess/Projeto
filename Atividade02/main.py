dd176c97149a3f798646a596ab30ebd3b165caeb
nathalylopess <lopesnathaly1810@gmail.com>
2025-05-09 15:24:29 -0300

    Primeira Versão da tarefa02

---
+++ Atividade02/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, HTTPException
+from models import Tarefa
+from typing import List
+
+app = FastAPI() # a partir dele serão criadas as rotas
+
+tarefas:List[Tarefa]=[] # tipando a variávvel para receber apenas itens desse tipo
+
+
+@app.get("/tarefas/",response_model=List[Tarefa])
+def listar_tarefas():
+    return tarefas
+
+@app.get("/tarefas/{id}",response_model=Tarefa)
+def listar_tarefa(id:int):
+    for tarefa in tarefas:
+        if tarefa.id == id:
+            return tarefa
+    raise HTTPException(status_code=404,detail="Não Localizado")
+
+@app.post("/tarefas/", response_model=Tarefa)
+def criar_tarefa(tarefa:Tarefa):
+    tarefas.append(tarefa)
+    return tarefa 
+''' |
+    |
+    v
+    Verificar se esse tipo que eu estou retornando é compatível com o response_model,
+    se não for, vai dar o erro 500, significando que os tipos não são compatíveis.
+'''
+   
---
+++ Atividade02/models.py
@@ -0,0 +1,7 @@
+from pydantic import BaseModel
+
+class Tarefa(BaseModel):
+    id:int
+    descricao:str
+    prioridade:int
+    concluida:bool
